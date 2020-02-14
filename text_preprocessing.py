4c168c2f3991fb01ee72662d1f1bd49962e89baa
catcooc <35857366+catcooc@users.noreply.github.com>
2020-02-14 21:16:03 +0800

    Create text_preprocessing.py

---
+++ text_preprocessing.py
@@ -0,0 +1,40 @@
+class Vocab(object):
+    def __init__(self, tokens, min_freq=0, use_special_tokens=False):
+        counter = count_corpus(tokens)  # : 
+        self.token_freqs = list(counter.items())
+        self.idx_to_token = []
+        if use_special_tokens:
+            # padding, begin of sentence, end of sentence, unknown
+            self.pad, self.bos, self.eos, self.unk = (0, 1, 2, 3)
+            self.idx_to_token += ['', '', '', '']
+        else:
+            self.unk = 0
+            self.idx_to_token += ['']
+        self.idx_to_token += [token for token, freq in self.token_freqs
+                        if freq >= min_freq and token not in self.idx_to_token]
+        self.token_to_idx = dict()
+        for idx, token in enumerate(self.idx_to_token):
+            self.token_to_idx[token] = idx
+
+    def __len__(self):
+        return len(self.idx_to_token)
+
+    def __getitem__(self, tokens):
+        if not isinstance(tokens, (list, tuple)):
+            return self.token_to_idx.get(tokens, self.unk)
+        return [self.__getitem__(token) for token in tokens]
+
+    def to_tokens(self, indices):
+        if not isinstance(indices, (list, tuple)):
+            return self.idx_to_token[indices]
+        return [self.idx_to_token[index] for index in indices]
+
+def count_corpus(sentences):
+    tokens = [tk for st in sentences for tk in st]
+    return collections.Counter(tokens)  # 返回一个字典，记录每个词的出现次数
+import spacy
+nlp = spacy.load('en_core_web_sm')
+doc = nlp(text)
+print([token.text for token in doc])
+from nltk.tokenize import word_tokenize
+print(word_tokenize(text))
